28da56f31b1ba77b85b42c8ff4ee0c713b0b850f
svelezpernett <89142690+svelezpernett@users.noreply.github.com>
2022-02-24 07:41:08 -0500

    Se agrega diciones utilizadas en python

---
+++ Decisiones.py
@@ -0,0 +1,55 @@
+'''
+numero = int(input("Porfavor digita un numero: "))
+
+print(f'El numero es {numero}')
+
+if(numero >= 0):
+    print("El numero es positivo")
+else:
+    print("El numero es negativo")
+'''
+
+
+'''
+nivel = int(input("Digite el nivel del agua: "))
+
+if(nivel < 200):
+    print("Hay muy poca agua en la represa")
+elif(nivel >= 200 and nivel < 450):
+    print("El nivel del agua es el adecuado")
+else:
+    print("Hay mucha agua, cuidado")
+'''
+
+
+'''
+mes = int(input("Porfvaor digite el mes del año (En numero): "))
+
+if(mes >= 1 and mes <=3):
+    print("Estamos en Invierno")
+elif(mes >= 4 and mes <= 6):
+    print("Estamos en Verano")
+elif(mes >= 7 and mes <= 9):
+    print("Estamos en Otoño")
+elif(mes >= 10 and mes <= 12):
+    print("Estamos Primavera")
+else:
+    print("Porfavor digite un numero valido del 1 al 12, los meses del año")
+'''
+
+
+#bucles/ciclos/repeticion/loops
+
+contador = 0
+
+while(contador < 100):
+    contador += 10
+    print(contador)
+    
+
+
+
+
+
+
+
