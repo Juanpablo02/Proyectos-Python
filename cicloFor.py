10c0fca5548c9ec3b76069f0d2e634f863b6cac1
svelezpernett <89142690+svelezpernett@users.noreply.github.com>
2022-03-03 07:39:34 -0500

    Ciclo for, con arrays

---
+++ cicloFor.py
@@ -0,0 +1,13 @@
+for i in range(20):
+    print(i)
+    
+    
+#Trabajo con arreglos
+
+nombres=["juna","jose","sergio"]
+print(nombres)
+
+# recorrer con for una lista en py
+
+for nombre in nombres:
+    print(nombre)
